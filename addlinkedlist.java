3b8aa70b901a2195f8248e25e15d8d3db61ea3af
Soumi Biswas <109500910+mohor12@users.noreply.github.com>
2023-07-02 20:53:09 +0530

    Create addlinkedlist.java

---
+++ addlinkedlist.java
@@ -0,0 +1,60 @@
+class LL{
+    Node head;
+    class Node{
+        String data;
+        Node next;
+        Node(String data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public void addFirst(String data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=newNode;
+            return;
+        }
+        newNode.next=head;
+        head=newNode;
+    }
+    public void addLast(String data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=newNode;
+            return;
+        }
+        Node currNode=head;
+        while(currNode.next!=null){
+            currNode=currNode.next;
+        }
+        currNode.next=newNode;
+    }
+    public void printList(){
+        if(head==null){
+            System.out.println("list is empty");
+            return;
+        }
+        Node currNode=head;
+        while(currNode!=null){
+            System.out.print(currNode.data+"->");
+            currNode=currNode.next;
+        }
+        System.out.println("Null");
+    }
+    public static void main(String args[]){
+        LL list=new LL();
+        list.addFirst("a");
+        list.addFirst("is");
+        list.printList();
+        list.addLast("List");
+        list.printList();
+        list.addFirst("this");
+        list.printList();
+    }
+    
+}
+
+output:
+is->a->Null
+is->a->List->Null
+this->is->a->List->Null
